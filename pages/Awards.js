a7a952aa6e214052405d8672d6ace29dfc6dcc5b
Arijit panda <73696583+ArzitPanda@users.noreply.github.com>
2022-07-22 22:00:26 +0530

    Update Awards.js

--- pages/Awards.js
+++ pages/Awards.js
@@ -19,9 +19,9 @@ const Awards = () => {
     <Heading c={'awards'} to="awards" form={'home'}/>
         <div className='px-1 md:px-10 grid grid-cols-1 grid-rows-4 lg:grid-cols-2 lg:grid-rows-2 items-center gap-x-9'>
 
-        {Cert.map((ele,key)=>{return(<div className="text-yellow-50  h-96 md:h-80  text-base md:text-lg my-7 bg-slate-800 rounded-xl lg:p-4 flex flex-col items-center justify-center">
+        {Cert.map((ele,key)=>{return(<div key={key} className="text-yellow-50  h-96 md:h-80  text-base md:text-lg my-7 bg-slate-800 rounded-xl lg:p-4 flex flex-col items-center justify-center">
 
-                <img src={ele.imgLink}  className="self-center flex-shrink-0 rounded-lg w-3/5 h-2/5 md:h-3/5 border object-contain rounded-sm md:justify-self-start shadow-xl "/>
+                <img src={ele.imgLink}  className="self-center flex-shrink-0  w-3/5 h-2/5 md:h-3/5 border object-contain rounded-sm md:justify-self-start shadow-xl "/>
                 <div className="flex flex-col h-36 p-2">
 			<h4 className="text-lg font-semibold text-center md:text-left ">{ele.head}</h4>
 			<p className="text-gray-400 text-center md:text-left">{ele.desc}</p>
