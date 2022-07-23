2c9e52fc3135471e3373ea95828bc6d9c895e6b7
Arijit panda <73696583+ArzitPanda@users.noreply.github.com>
2022-07-23 19:54:51 +0530

    update

--- components/Pricing.js
+++ components/Pricing.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Navbar from './Navbar'
+// import Navbar from './Navbar'
 import PriceCard from './PriceCard'
 
 const Pricing = () => {
   return (
 
 <div>
-	<Navbar/>
+	{/* <Navbar/> */}
 	<div className="flex flex-col lg:flex-row w-screen mt-12 items-center justify-center  gap-y-7  md:gap-x-7 flex-wrap p-10 ">
 	<PriceCard/>
 	<PriceCard/>
--- pages/Price.js
+++ pages/Price.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import FooterBot from '../components/FooterBot'
+import NavBar from '../components/NavBar'
 import Pricing from '../components/Pricing'
 import Table from '../components/Table'
 
 const Price = () => {
   return (
-    <div className="bg-slate-800"><Pricing/>
-    
+    <div className="bg-slate-800">
+      <NavBar/>
+      <Pricing/>
+    <FooterBot/>
     
     </div>
   )
